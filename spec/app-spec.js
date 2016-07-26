351a2faab5eeca4ba57e0bfd25e0a552ef406bcf
yhl221 <1727538058@qq.com>
2016-07-26 13:47:01 +0800

    add test to app-spec.js

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -3,9 +3,16 @@ const CompareNumber = require('../src/compare-number.js');
 describe('guess number game', ()=> {
     let randomNumber = '1234';
     let firstInput = '1234';
+    let secondInput = '4321';
     it('compare firstNumber to random', ()=> {
         const resultFirst = CompareNumber.compareNumber(randomNumber, firstInput);
         const expectFirstReault = '4A0B';
         expect(resultFirst).toEqual(expectFirstReault);
+    });
+
+    it('compare secondNumber to random', ()=> {
+        const resultSecond = CompareNumber.compareNumber(randomNumber, secondInput);
+        const expectSecondResult = '0A4B';
+        expect(resultSecond).toEqual(expectSecondResult);
     });
 });
