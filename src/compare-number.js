5b80428a568c11a7f29d800909563aee9fabde48
yhl221 <1727538058@qq.com>
2016-07-26 13:45:34 +0800

    create CompareNumber class

---
+++ src/compare-number.js
@@ -0,0 +1,28 @@
+'use strict';
+class CompareNumber {
+
+    constructor(random, guess) {
+        this.random = random;
+        this.guess = guess;
+    }
+
+    static compareNumber(random, guess) {
+        let randomArray = Array.from(random);
+        let guessArray = Array.from(guess);
+        let numberAndPostTrue = 0;
+        let numberTrueAndPosFalse = 0;
+        for (const random of randomArray) {
+            let findNumber = guessArray.some(e=>e === random);
+            if (findNumber) {
+                if (randomArray.indexOf(random) === guessArray.indexOf(random))
+                    numberAndPostTrue++;
+                else
+                    numberTrueAndPosFalse++;
+            }
+        }
+
+        return `${numberAndPostTrue}A${numberTrueAndPosFalse}B`;
+    }
+}
+
+module.exports = CompareNumber;
